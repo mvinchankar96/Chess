480fbfc26202d00b42d7b08c420915554bf86ee8
Mangesh Vinchankar <mv091096@gmail.com>
2025-04-19 15:49:59 +0530

    [Mangesh] Update ChessPositionCalculator with Pawn and position class and update tests

--- src/main/java/com/chess/ChessPositionCalculator.java
+++ src/main/java/com/chess/ChessPositionCalculator.java
@@ -1,10 +1,13 @@
 package com.chess;
 
+import com.chess.domain.Pawn;
+import com.chess.domain.Position;
+
 import java.util.List;
 
 public class ChessPositionCalculator {
 
-    public List<String> calculate(String pawn, String position) {
-        return List.of("H8");
+    public List<Position> calculate(Pawn pawn, Position position) {
+        return pawn.getPossibleMoves(position);
     }
 }
--- src/test/java/com/chess/ChessPositionCalculatorTest.java
+++ src/test/java/com/chess/ChessPositionCalculatorTest.java
@@ -1,16 +1,30 @@
 package com.chess;
 
-import org.junit.jupiter.api.Assertions;
+import com.chess.domain.Pawn;
+import com.chess.domain.Position;
+import com.chess.exception.InvalidPositionException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ChessPositionCalculatorTest {
 
     @Test
     void shouldCalculatePossibleMovesGivenPositionForPawn() {
         ChessPositionCalculator chessPositionCalculator = new ChessPositionCalculator();
-        List<String> possibleMoves = chessPositionCalculator.calculate("Pawn", "H7");
-        Assertions.assertEquals(List.of("H8"), possibleMoves);
+        List<Position> possibleMoves = chessPositionCalculator.calculate(new Pawn(), Position.from("H7"));
+        assertEquals(List.of(Position.from("H8")), possibleMoves);
+    }
+
+    @Test
+    void shouldThrowExceptionGivenInvalidPositionToCalculateMoves() {
+        ChessPositionCalculator chessPositionCalculator = new ChessPositionCalculator();
+
+        InvalidPositionException invalidPositionException = assertThrows(InvalidPositionException.class, () -> chessPositionCalculator.calculate(new Pawn(), Position.from("H9")));
+
+        assertEquals(invalidPositionException.getMessage(), "Rank '9' is out of bounds. Only ranks 1 to 8 are supported.");
     }
 }
