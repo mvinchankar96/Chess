9c4b88f73e9d091e791714c84941f76c5701af67
Mangesh Vinchankar <mv091096@gmail.com>
2025-04-19 15:14:51 +0530

    [Mangesh] Updates Position.java with validation of position string

--- src/main/java/com/chess/domain/Position.java
+++ src/main/java/com/chess/domain/Position.java
@@ -1,16 +1,32 @@
 package com.chess.domain;
 
+import com.chess.exception.InvalidPositionException;
+
+import static java.util.Objects.isNull;
+
 public record Position(int file, int rank) {
 
     public static final char START_FILE_CHAR = 'A';
 
     public static Position from(String position) {
+        if (isNull(position) || position.length() != 2) {
+            throw new InvalidPositionException("Invalid position: " + position);
+        }
+
         char fileChar = Character.toUpperCase(position.charAt(0));
         char rankChar = position.charAt(1);
 
         int fileIndex = fileChar - START_FILE_CHAR;
         int rankIndex = Character.getNumericValue(rankChar) - 1;
 
+        if (withinLimits(fileIndex) || withinLimits(rankIndex)) {
+            throw new InvalidPositionException("Position out of bounds: " + position);
+        }
+
         return new Position(fileIndex, rankIndex);
+    }
+
+    private static boolean withinLimits(int fileIndex) {
+        return fileIndex < 0 || fileIndex >= 8;
     }
 }
--- src/test/java/com/chess/domain/PositionTest.java
+++ src/test/java/com/chess/domain/PositionTest.java
@@ -1,13 +1,40 @@
 package com.chess.domain;
 
+import com.chess.exception.InvalidPositionException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PositionTest {
     @Test
     void shouldReturnPositionWithFileAndRank() {
         assertEquals(new Position(0, 0), Position.from("A1"));
         assertEquals(new Position(7, 0), Position.from("H1"));
+    }
+
+    @Test
+    void shouldThrowExceptionGivenInvalidPositionForFile() {
+        InvalidPositionException invalidPositionException = assertThrows(InvalidPositionException.class, () -> Position.from("J9"));
+        assertEquals("Position out of bounds: J9", invalidPositionException.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionGivenInvalidPositionForRank() {
+        InvalidPositionException invalidPositionException = assertThrows(InvalidPositionException.class, () -> Position.from("A9"));
+        assertEquals("Position out of bounds: A9", invalidPositionException.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionGivenPositionAsNull() {
+        InvalidPositionException invalidPositionException = assertThrows(InvalidPositionException.class, () -> Position.from(null));
+        assertEquals("Invalid position: null", invalidPositionException.getMessage());
+
+    }
+
+    @Test
+    void shouldThrowExceptionGivenInvalidPosition() {
+        InvalidPositionException invalidPositionException = assertThrows(InvalidPositionException.class, () -> Position.from("invalid"));
+        assertEquals("Invalid position: invalid", invalidPositionException.getMessage());
     }
 }
